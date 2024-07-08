f015b3fc1430b213748bd926c6ec8493cb7e1dcb
krabapples <koen.vanderpoorten@gmail.com>
2024-07-08 16:48:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id   = "<azure_subscription_id>"
   tenant_id         = "<azure_subscription_tenant_id>"
   client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  client_secret     = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
